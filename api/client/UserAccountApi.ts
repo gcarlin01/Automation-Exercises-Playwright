abcdb4725f84c7ed51736da4046688d327c98267
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-02-06 18:33:10 -0500

    adds fake value to myUser test and client util still in progress (userAccount.spec failing)

--- api/client/UserAccountApi.ts
+++ api/client/UserAccountApi.ts
@@ -2,14 +2,19 @@ import { APIRequestContext } from "@playwright/test";
 
 export class UserAccountApi {
   private request: APIRequestContext;
-  private productsListEndpoint = "/api/productsList";
+  private createAccountEndpoint = "/api/createAccount";
+  private updateAccountEndpoint = "/api/updateAccount";
+  private getAccountEndpoint = "/api/getUserDetailByEmail";
+  private deleteAccountEndpoint = "/api/deleteAccount";
 
   constructor(request: APIRequestContext) {
     this.request = request;
   }
 
-  public async Get() {
-    const response = await this.request.get(this.productsListEndpoint);
+  public async Post(UserCreationParams: any) {
+    const response = await this.request.post(this.createAccountEndpoint, {
+      form: UserCreationParams,
+    });
     const responseBody = await response.text();
 
     return {
@@ -18,8 +23,34 @@ export class UserAccountApi {
     };
   }
 
-  public async Post() {
-    const response = await this.request.post(this.productsListEndpoint);
+  public async Put(UserCreationParams: any) {
+    const response = await this.request.put(this.updateAccountEndpoint, {
+      form: UserCreationParams,
+    });
+    const responseBody = await response.text();
+
+    return {
+      status: response.status(),
+      body: JSON.parse(responseBody),
+    };
+  }
+
+  public async Get(email: string) {
+    const response = await this.request.get(this.getAccountEndpoint, {
+      form: { email: email },
+    });
+    const responseBody = await response.text();
+
+    return {
+      status: response.status(),
+      body: JSON.parse(responseBody),
+    };
+  }
+
+  public async Delete(email: string, password: string) {
+    const response = await this.request.delete(this.deleteAccountEndpoint, {
+      form: { email: email, password: password },
+    });
     const responseBody = await response.text();
 
     return {
--- api/tests/userAccount.spec.ts
+++ api/tests/userAccount.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@playwright/test";
 import { faker } from "@faker-js/faker";
+import { UserAccountApi } from "../client/UserAccountApi";
 
 test.describe("user account management", () => {
   let myUser;
@@ -7,17 +8,67 @@ test.describe("user account management", () => {
   test("can create, read, update, and delete a user account", async ({
     request,
   }) => {
-    const myUser = { name: faker.internet.email() };
+    myUser = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      title: faker.person.prefix(),
+      birth_day: faker.number.int({ min: 1, max: 28 }),
+      birth_month: faker.number.int({ min: 1, max: 12 }),
+      birth_year: faker.number.int({ min: 1900, max: 2010 }),
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      company: faker.company.name(),
+      address1: faker.location.streetAddress(),
+      address2: faker.location.secondaryAddress(),
+      country: faker.location.country(),
+      zipcode: faker.location.zipCode(),
+      state: faker.location.state(),
+      city: faker.location.city(),
+      mobile_number: faker.phone.number(),
+    };
 
-    test.step("create a user", () => {
+    test.step("create a user", async () => {
+      const api = new UserAccountApi(request);
+      const response = await api.Post(myUser);
+      console.log(myUser);
+      expect(response.body).toEqual({
+        responseCode: 201,
+        message: "User created!",
+      });
       // post to create a user
     });
-    test.step("read the user", () => {
+
+    test.step("read the user", async () => {
+      const api = new UserAccountApi(request);
+
+      const response = await api.Get(myUser.email);
+      console.log(response.body);
+
+      expect(response.body.responseCode).toEqual(404);
+
       // get the user
     });
-    test.step("update the user", () => {
+    test.step("update the user", async () => {
+      const api = new UserAccountApi(request);
+
+      const response = await api.Put(myUser);
+      console.log(myUser);
+      expect(response.body).toEqual({
+        responseCode: 200,
+        message: "User updated!",
+      });
       // PUT to update the user
     });
-    test.step("delete the user", () => {});
+    test.step("delete the user", async () => {
+      const api = new UserAccountApi(request);
+
+      const response = await api.Delete(myUser.email, myUser.password);
+      console.log(myUser.email, myUser.password);
+      expect(response.body).toEqual({
+        responseCode: 200,
+        message: "Account deleted!",
+      });
+    });
   });
 });
